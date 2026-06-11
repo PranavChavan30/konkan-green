8feafebbd8ee74ab2b709a655f1669b581214747
Pranav Chavan <30chavanpranav@gmail.com>
2026-06-11 14:50:02 +0530

    Updated website

--- app.py
+++ app.py
@@ -2,36 +2,43 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'konkan_secret'
+app.config["SECRET_KEY"] = "konkan_secret"
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/products')
+
+@app.route("/products")
 def products():
-    return render_template('products.html')
+    return render_template("products.html")
 
-@app.route('/about')
+
+@app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template("about.html")
 
-@app.route('/events')
+
+@app.route("/events")
 def events():
-    return render_template('events.html')
+    return render_template("events.html")
 
-@app.route('/certificates')
-@app.route('/verify')
+
+@app.route("/certificates")
 def certificates():
-    return render_template('certificates.html')
+    return render_template("certificates.html")
 
-@app.route('/blog')
+
+@app.route("/blog")
 def blog():
-    return render_template('blog.html')
+    return render_template("blog.html")
 
-@app.route('/contact')
+
+@app.route("/contact")
 def contact():
-    return render_template('contact.html')
+    return render_template("contact.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
